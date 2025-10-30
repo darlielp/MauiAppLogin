3f1191d631eee4debdc6f5554925ff973f02a8b8
darliel <darlieldev.deed634@simplelogin.com>
2025-10-30 17:16:15 -0300

    Complementação do projeto

--- MauiAppLogin/Login.xaml.cs
+++ MauiAppLogin/Login.xaml.cs
@@ -6,4 +6,43 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			List<UsuarioCadastrados> lista_usuario = new List<UsuarioCadastrados>()
+			{ 
+				new UsuarioCadastrados()
+				{
+					Usuario = "jose",
+					Senha = "123"
+				},
+				new UsuarioCadastrados()
+				{
+					Usuario = "maria",
+					Senha = "321"
+				}
+			};
+
+			UsuarioCadastrados dados_digitados = new UsuarioCadastrados()
+			{
+				Usuario = usuario.Text,
+				Senha = senha.Text
+			};
+
+			if (lista_usuario.Any(i => (dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha)))
+			{
+				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+				App.Current.MainPage = new Protegida();
+			} else
+			{
+				throw new Exception("Senha ou Usuario Invalidos");
+			}
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "Fechar");
+		}
+    }
 }
